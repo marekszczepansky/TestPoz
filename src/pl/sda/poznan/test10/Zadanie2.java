10a99ebaf8c5b233f522e241b748204577631555
marekszczepansky <marek.szczepansky@gmail.com>
2021-09-19 14:35:46 +0200

    Zadanie2 added

---
+++ src/pl/sda/poznan/test10/Zadanie2.java
@@ -0,0 +1,29 @@
+package pl.sda.poznan.test10;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        byte b;
+        System.out.println(b = 68);
+
+        short s = 1000;
+        System.out.println(s);
+
+        int i = 2147483647;
+        System.out.println(i);
+
+        long l = 822337203;
+        System.out.println(l);
+
+        float f = 234.5f;
+        System.out.println(f);
+
+        double d = 123.4;
+        System.out.println(d);
+
+        boolean bo = true;
+        System.out.println(bo);
+
+        String st = "Software Development Academy";
+        System.out.println(st);
+    }
+}
