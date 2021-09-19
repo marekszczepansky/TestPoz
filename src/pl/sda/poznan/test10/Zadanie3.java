a8291cb72e7e0fce9c9f9b79661efd84d8532dd8
marekszczepansky <marek.szczepansky@gmail.com>
2021-09-19 14:39:24 +0200

    Zadanie3 added

---
+++ src/pl/sda/poznan/test10/Zadanie3.java
@@ -0,0 +1,14 @@
+package pl.sda.poznan.test10;
+
+/**
+ * int -> i = 100000000000000000000000
+ * long -> l = -900000000000000000000000
+ * double -> d = 100
+ */
+public class Zadanie3 {
+    public static void main(String[] args) {
+        int i = 100000000000000000000000;
+        long l = -900000000000000000000000L;
+        double d = 100;
+    }
+}
