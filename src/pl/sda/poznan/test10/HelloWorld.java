49b4cbab06892959a96f7b94516a9dd0f24f3e9d
marekszczepansky <marek.szczepansky@gmail.com>
2021-09-19 15:57:43 +0200

    update

--- src/pl/sda/poznan/test10/HelloWorld.java
+++ src/pl/sda/poznan/test10/HelloWorld.java
@@ -13,11 +13,21 @@ public class HelloWorld {
         int x, y;
         boolean a = true, b = false;
 
+        System.out.println(myScanner instanceof Scanner);
+        System.out.println(myScanner instanceof Object);
+
         System.out.println("Podaj wartość x ");
         x = myScanner.nextInt();
         System.out.println("Podaj wartość y ");
         y = myScanner.nextInt();
 
+        String password = myScanner.nextLine();
+
+        boolean testSecret = "secret".equals(password);
+        boolean testSecret2 = password != null && password.equals("secret");
+
+        int larger = x > y ? x : y;
+        System.out.println("Większa liczba to: " + larger);
 
         System.out.println(!(a && b) + " = " + (!a || !b));
         System.out.println(!(a || b) + " = " + (!a && !b) );
@@ -46,6 +56,11 @@ public class HelloWorld {
         System.out.println("~y = " + ~y);
         System.out.println("~x    = "  + displayBinary(~x));
         System.out.println("~y    = "  + displayBinary(~y));
+        System.out.println();
+        System.out.println("x     = "  + displayBinary(x));
+        System.out.println("x<<3  = "  + displayBinary(x<<3));
+        System.out.println("x>>3  = "  + displayBinary(x>>3));
+        System.out.println("x>>>3 = "  + displayBinary(x>>>3));
 
     }
 
