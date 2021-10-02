842a31e07812799a5d36e2eb11f628a40a218aec
marekszczepansky <marek.szczepansky@gmail.com>
2021-10-02 13:57:20 +0200

    zadanie 11

---
+++ src/pl/sda/poznan/test10/Zadanie11.java
@@ -0,0 +1,27 @@
+package pl.sda.poznan.test10;
+
+import java.util.Scanner;
+
+/**
+ * Napisz program, który pobiera hasło od użytkownika.
+ * Program powinien zapytać użytkownika o wprowadzenie poprawnego hasła.
+ * W przypadku gdy wprowadzone hasło będzie poprawne, program powinien
+ * wydrukować na ekranie tekst "Poprawne hasło", w przeciwnym wypadku,
+ * wprowadzone hasło powinno zostać wyświetlone na ekranie, a użytkownik
+ * powinien mieć możliwość ponownie wprowadzić hasło.
+ */
+public class Zadanie11 {
+
+    public static final String SECRET = "secret";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String password;
+        do {
+            System.out.println("Podaj hasło");
+            password = scanner.nextLine();
+            System.out.println(SECRET.equals(password) ? "Poprawne hasło" : password);
+        } while (!SECRET.equals(password));
+
+    }
+}
