39e009d04aecca125d73f9cf16d63a4fdff14671
marekszczepansky <marek.szczepansky@gmail.com>
2021-10-02 15:33:49 +0200

    zadanie 18

--- src/pl/sda/poznan/test10/Zadanie18.java
+++ src/pl/sda/poznan/test10/Zadanie18.java
@@ -13,11 +13,11 @@ public class Zadanie18 {
         participantList[1] = "Blake Baker";
         participantList[2] = "Jacob Campbell";
         participantList[3] = "Arthur Green";
-        participantList[5] = "Caleb Willis";
+        participantList[4] = "Caleb Willis";
         participantList[5] = "Heath Rasmussen";
         participantList[6] = "Lara Malinowska";
         participantList[7] = "Kase Knapp";
-        participantList[7] = "Avery Livingston";
+        participantList[8] = "Avery Livingston";
         participantList[9] = "Armando Zamora";
         participantList[10] = "Skye Hunt";
         participantList[11] = "Gale Carr";
@@ -25,7 +25,7 @@ public class Zadanie18 {
         participantList[13] = "Mel Pruitt";
         participantList[14] = "Jess Owen";
 
-        for (int i = 0; 11 > i; i++ ) {
+        for (int i = 0; participantList.length > i; i++ ) {
             System.out.println("Uczestnik nr " + i + " to " + participantList[i]);
         }
     }
